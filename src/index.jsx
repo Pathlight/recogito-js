a0c68a63bdc8efd26a7fb3445dbf9346aecac9f2
vorpal56 <anthonysukadil1@gmail.com>
2022-06-06 17:13:18 -0400

    Load annotations using 'web_annotation_data'

--- src/index.jsx
+++ src/index.jsx
@@ -134,7 +134,7 @@ export class Recogito {
 
   getAnnotations = () => {
     const annotations = this._app.current.getAnnotations();
-    return annotations.map(a => a.underlying);
+    return annotations.map(a => ({...a.underlying, creator: a.creator}));
   }
 
   loadAnnotations = (url, requestArgs) => fetch(url, requestArgs)
@@ -166,7 +166,9 @@ export class Recogito {
 
   setAnnotations = arg => {
     const annotations = arg || [];
-    const webannotations = annotations.map(a => new WebAnnotation(a));
+    const webannotations = annotations.map(
+      ({web_annotation_data, creator}) => new WebAnnotation(web_annotation_data, {creator})
+    );
     return this._app.current.setAnnotations(webannotations);
   }
 

c1f58ad5227f520267551ab43ca771f20890a22f
vorpal56 <anthonysukadil1@gmail.com>
2022-06-15 12:42:53 -0400

    Include react/react-dom as peer dependencies and exclude in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -25,9 +25,20 @@ module.exports = {
   },
   resolve: {
     extensions: ['.js', '.jsx'],
-    alias: {
-      'react': path.resolve('node_modules/react')
-    }
+  },
+  externals: {
+    'react': {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'React',
+      root: 'React'
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'ReactDOM',
+      root: 'ReactDOM'
+    }    
   },
   module: {
     rules: [
